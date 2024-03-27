652ac0867366b07d2aa4d1aa16107df9240ec26b
Rutu Bhatt <164050514+Rutu110@users.noreply.github.com>
2024-03-28 01:11:56 +0530

    Create pythonhr.jsx

---
+++ src/components/pythonhr/pythonhr.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pythonhr = () => {
+  return (
+    <iframe
+    title="Streamlit App"
+    src="http://localhost:8503/"
+    width="100%"
+    height="800px"
+  />
+  )
+}
+
+export default Pythonhr
