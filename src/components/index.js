13fbf37765075ef83209a0d02ab650482689974f
Rutu Bhatt <164050514+Rutu110@users.noreply.github.com>
2024-03-28 01:15:01 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+export { default as Article} from './article/Article';
+export { default as Brand} from './brand/Brand';
+export { default as Feature} from './feature/Feature';
+export { default as Cta} from './cta/Cta';
+export { default as Navbar} from './navbar/Navbar';
+export {default as Login} from './login/Login';
+export {default as Register} from './register/Register';
+export {default as Home} from './home/Home';
+export {default as Cr} from './cr/Cr';
+export {default as Profile} from './profile/Profile';
+export {default as Rapython} from './rapython/Rapython';
+export {default as Pythoncs} from './pythoncs/Pythoncs';
+export {default as Pythonhr} from './pythonhr/Pythonhr';
+
+
+
+
+
