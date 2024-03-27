6a57720af7a003b1a7f57e1febc2c93c8ee751be
Rutu Bhatt <164050514+Rutu110@users.noreply.github.com>
2024-03-28 00:38:34 +0530

    Create cr.jsx

---
+++ src/components/cr.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Body from './Body/Body';
+import './cr.css';
+
+const Cr = () => {
+  return (
+    <div className='cr'>
+       <Body />
+    </div>
+  )
+}
+
+export default Cr;
