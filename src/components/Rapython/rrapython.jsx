005c97ff51503b53f0502ee83f10eff9ac01087d
Rutu Bhatt <164050514+Rutu110@users.noreply.github.com>
2024-03-28 01:12:58 +0530

    Create rrapython.jsx

---
+++ src/components/Rapython/rrapython.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rapython = () => {
+  return (
+    <iframe
+    title="Streamlit App"
+    src="http://localhost:8501/"
+    width="100%"
+    height="800px"
+  />
+  )
+}
+
+export default Rapython;
