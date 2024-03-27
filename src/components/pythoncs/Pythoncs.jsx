3cf3112e25a9f26e42eca149790e78da88aac678
Rutu Bhatt <164050514+Rutu110@users.noreply.github.com>
2024-03-28 01:07:47 +0530

    Create Pythoncs.jsx

---
+++ src/components/pythoncs/Pythoncs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pythoncs = () => {
+  return (
+    <iframe
+    title="Streamlit App"
+    src="http://localhost:8502/"
+    width="100%"
+    height="800px"
+  />
+  )
+}
+
+export default Pythoncs
